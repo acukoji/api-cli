d2c18711363bfba62149257e0d141aafa5fa921b
Acukoji <acukoji@gmail.com>
2019-10-03 16:32:35 -0600

    working on commanderjs

--- index.js
+++ index.js
@@ -8,19 +8,68 @@
 const program = require('commander');
 
 program
-  .option('-d, --debug', 'output extra debugging of our app')
-  // .option('-s, --small', 'small pizza size')
-  .option('-p, --pizza-type <type>', 'flavour of pizza');
+//  .option('-s, --small', 'small pizza size')
+  .option('-p, --pizza-type <type>', 'flavour of pizza')
+  .option('-s, --pizza-size <type>', 'size of pizza');
 
-// console.log(process.argv);
+//console.log(process.argv);
 program.parse(process.argv);
 
-if (program.debug) {
-  console.log(program.opts());
-}
-// console.log('pizza details:');
-// if (program.small) console.log('- small pizza size');
+console.log(program.opts());
+// if (program.moreInfo) {
+//   console.log(program.opts());
+// } 
 
-if (program.pizzaType) {
-  console.log(`- ${program.pizzaType}`);
-}
+
+/*const program = require('commander');
+
+program
+  .option('-c, --cheese <type>', 'add the specified type of cheese', 'blue');
+
+program.parse(process.argv);
+
+console.log(`cheese: ${program.cheese}`);
+*/
+
+/*const program = require('commander');
+
+program
+  .option('--no-sauce', 'Remove sauce')
+  .option('--cheese <flavour>', 'cheese flavour', 'mozzarella')
+  .option('--no-cheese', 'plain with no cheese')
+  .parse(process.argv);
+
+const sauceStr = program.sauce ? 'sauce' : 'no sauce';
+const cheeseStr = (program.cheese === false) ? 'no cheese' : `${program.cheese} cheese`;
+console.log(`You ordered a pizza with ${sauceStr} and ${cheeseStr}`);
+*/
+
+/*
+const program = require('commander');
+
+program
+  .option('-c, --cheese [type]', 'Add cheese with optional type');
+
+program.parse(process.argv);
+
+if (program.cheese === undefined) console.log('no cheese');
+else if (program.cheese === true) console.log('add cheese');
+else console.log(`add cheese type ${program.cheese}`);
+*/
+
+
+
+// const program = require('commander');
+
+// function commaSeparatedList(value, dummyPrevious) {
+//   return value.split(',');
+// }
+
+// program
+//   .option('-l, --list <items>', 'comma separated list', commaSeparatedList);
+
+// console.log(process.argv);
+
+// program.parse(process.argv);
+
+// if (program.list !== undefined) console.log(program.list);
