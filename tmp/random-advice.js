5b311093e8fb1261a15c15e484ea61cd7828e039
Acukoji <acukoji@gmail.com>
2019-10-03 19:02:17 -0600

    working with axios

---
+++ tmp/random-advice.js
@@ -0,0 +1,28 @@
+const axios = require('axios');
+
+async function random3Advice() {
+    try {
+        const first = await axios.get('https://api.adviceslip.com/advice');
+        const sec = await axios.get('https://api.adviceslip.com/advice');
+        const third = await axios.get('https://api.adviceslip.com/advice');
+        console.log([first.data, sec.data, third.data]);
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+async function random3AdviceAll() {
+    try {
+        const responses = await axios.all([
+            axios.get('https://api.adviceslip.com/advice'),
+            axios.get('https://api.adviceslip.com/advice'),
+            axios.get('https://api.adviceslip.com/advice')
+        ])
+        console.log(responses.map(r => r.data));
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+random3AdviceAll()
+console.log("testing...");
