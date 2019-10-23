1d1c0bd77975602d46d28c3a3c2797aea81c5603
Acukoji <acukoji@gmail.com>
2019-10-22 20:42:22 -0600

    added repeat3 funcRunner test

--- tests/funcRunner.test.ts
+++ tests/funcRunner.test.ts
@@ -1,6 +1,6 @@
 import { funcRunner } from '../src/funcRunner';
 
-test('test reverseString', () => {
+test('test funcRunner calling reverseString', () => {
     function reverseString(str: string): string {
         return str.split("").reverse().join("");
     }
@@ -9,8 +9,16 @@ test('test reverseString', () => {
     expect(result).toEqual('olleh');
 });
 
-
-test('test parseInt', () => {
+test('test funcRunner calling parseInt', () => {
     const result = funcRunner('12', parseInt);
     expect(result).toEqual(12);
+});
+
+test('test funcRuner calling repeat3x', () => {
+    function repeat3x (str: string): string{
+        return str + str + str;
+    }
+
+    const result = funcRunner('print', repeat3x);
+    expect(result).toEqual('printprintprint');
 });
