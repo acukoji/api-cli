61af7d4c5f54b1a7f81b89d5417b979aa8fa6984
Acukoji <acukoji@gmail.com>
2019-10-22 19:34:40 -0600

    updated code; replaced 'it' with 'test' function for jest

--- tests/fib.test.ts
+++ tests/fib.test.ts
@@ -1,17 +1,17 @@
 import { fib } from '../src/fib';
 
-it('test fib(0) returns 0', () => {
+test('test fib(0) returns 0', () => {
     expect(fib(0)).toEqual(0);
 });
 
-it('test fib(1) returns 1', () => {
+test('test fib(1) returns 1', () => {
     expect(fib(1)).toEqual(1);
 });
 
-it('test fib(2) returns 1', () => {
+test('test fib(2) returns 1', () => {
     expect(fib(2)).toEqual(1);
 });
 
-it('test fib(14) returns 377', () => {
+test('test fib(14) returns 377', () => {
     expect(fib(14)).toEqual(377);
 });
