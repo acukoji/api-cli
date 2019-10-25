3e82cafff1391bd37e436224125c054ea54a4884
Acukoji <acukoji@gmail.com>
2019-10-25 00:27:35 -0600

    Adding Solution to print-word-count.

--- src/tmp/print-word-count.ts
+++ src/tmp/print-word-count.ts
@@ -1,6 +1,40 @@
 function printWordCount(str: string): void {
-    console.log("testing");    
+
+    var CountAlpha = 0;
+    var CountBeta = 0;
+    var CountOmega = 0;
+
+    var wordsArray = str.split(' ');
+    //console.log(wordsArray);
+    var i = 0;
+    for (i = 0; i < wordsArray.length; i++) {
+        if (wordsArray[i] == 'alpha') {
+            CountAlpha = CountAlpha + 1;
+        }
+        else if (wordsArray[i] == 'beta') {
+            CountBeta = CountBeta + 1;
+        }
+        else {
+            CountOmega = CountOmega + 1;
+        }
+    }
+    console.log('Alpha: ' + CountAlpha);
+    console.log('Beta: ' + CountBeta);
+    console.log('Omega: ' + CountOmega);
 }
-
-
 printWordCount('alpha alpha beta beta omega beta')
+
+/*
+resources used:
+https://stackoverflow.com/questions/52015844/how-to-convert-word-list-to-array-in-javascript
+--for creating an array of words using str.split.
+
+https://www.w3schools.com/js/js_loop_for.asp
+--reviewed how to set up for loops.
+
+How I wrote the code:
+--I knew i needed to contain the str parameter in an array so i could count them based on index position.
+--I looked up a way to split the string into an array using .split
+--I knew I needed to iterate over the array, so i looked up for loops
+--I knew I needed a way to flag/count each instance of each word, so I set up a variable for word-count, and +1'd them with an if statement.
+*/
