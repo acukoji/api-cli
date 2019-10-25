bfdc69d92d3d1afea6f9ee428393ee69c8ea5b18
Acukoji <acukoji@gmail.com>
2019-10-25 12:40:21 -0600

    V2 of print-word-count--long form.

--- src/tmp/print-word-count.ts
+++ src/tmp/print-word-count.ts
@@ -1,32 +1,36 @@
 function printWordCount(str: string): void {
 
-    var CountAlpha = 0;
-    var CountBeta = 0;
-    var CountOmega = 0;
-
     var wordsArray = str.split(' ');
-    //console.log(wordsArray);
-    var i = 0;
-    for (i = 0; i < wordsArray.length; i++) {
-        if (wordsArray[i] == 'alpha') {
-            CountAlpha = CountAlpha + 1;
+    var sortedArray = wordsArray.sort();
+    // sort function makes it possible for for-loop to count repeated words, because all repeats are grouped together, and we structure the for-loop to include an if-statement that notices when current iteration (wordsArray[i]) is different than what came before (wordSeen).
+    var wordSeen = null; 
+    var wordCount = 0; 
+    // initializing wordSeen/count to null/zero keeps the first iteration from being counted and printed out.  The requirements of the if-statements below allow for the first instance to 'pass through'.  
+    for (var i = 0; i < wordsArray.length; i++) {
+        if (wordsArray[i] != wordSeen) {
+            //sets condition to print out wordSeen with its count, only once the for-loop iterates to a word that's new.
+            if (wordCount > 0){  //important bc wordSeen only becomes defined after the first pass; it starts out null, but is assigned array[i] below--when wordCount also become greater than 0.  Alternatively, if(wordSeen != null) would also work here.
+                console.log(wordSeen + ": " + wordCount);
+            }
+            wordSeen = wordsArray[i];
+            wordCount = 1; 
         }
-        if (wordsArray[i] == 'beta') {
-            CountBeta = CountBeta + 1;
-        }
-        if (wordsArray[i] == 'omega') {
-            CountOmega = CountOmega + 1;
+        else{   //wordCount increases each time wordsArray != wordSeen
+            wordCount ++;
         }
     }
-    console.log('alpha: ' + CountAlpha);
-    console.log('beta: ' + CountBeta);
-    console.log('omega: ' + CountOmega);
+//not sure why next line was in the example i modeled from. Seems unnecessary?  It works even in current commented out form.
+ //   if (wordCount > 0){ 
+        console.log(wordSeen + ": " + wordCount);
+ //   }
+
 }
-printWordCount('alpha alpha beta beta omega beta')
+//printWordCount('alpha alpha beta beta omega beta')
+printWordCount('1 22 22 1 3')
 
 /*
 resources used:
-https://stackoverflow.com/questions/52015844/how-to-convert-word-list-to-array-in-javascript
+https://stackoverflow.com/questions/52015844/how-to-convert-wordSeen-list-to-array-in-javascript
 --for creating an array of words using str.split.
 
 https://www.w3schools.com/js/js_loop_for.asp
@@ -36,5 +40,16 @@ How I wrote the code:
 --I knew i needed to contain the str parameter in an array so i could count them based on index position.
 --I looked up a way to split the string into an array using .split
 --I knew I needed to iterate over the array, so i looked up for loops
---I knew I needed a way to flag/count each instance of each word, so I set up a variable for word-count, and +1'd them with an if statement.
+--I knew I needed a way to flag/count each instance of each wordSeen, so I set up a variable for wordSeen-count, and +1'd them with an if statement.
+*/
+
+/* V2 resources used:
+https://stackoverflow.com/questions/34615493/count-duplicates-in-an-array
+
+V2 How I wrote the code:
+--I brainstormed what code was needed to track any new string which appeared.
+--I kept the initial code of .split, and added .sort.
+--I really wanted to figure it out on my own, but in googling for answers on how to figure out how to keep track of new words from an array, it was pretty much unavoidable to find the solution .
+--I attempted to understand what was going on in the "long way" code, and recreate it here instead of just copypasta from above link.
+--I added comments so that I could understand what was happening at each step, and also played with altering parts of the code to see if it would break, and to understand better what each line was doing. 
 */
