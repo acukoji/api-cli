f9a68d708d0e4552e4f3616c42c4637d105bae74
Acukoji <acukoji@gmail.com>
2020-01-09 16:39:55 -0700

    changed code to enhance readability

--- src/api/advice.ts
+++ src/api/advice.ts
@@ -2,21 +2,27 @@ import axios, { AxiosResponse } from 'axios';
 
 const ADVICE_API_RANDOM_URL = "https://api.adviceslip.com/advice";
 
-type Advice = {
-    advice: string;
-    slip_id: string;
-}
-
-type Slip = {
-    slip: Advice
+type SlipResponse = {
+    slip: {
+        advice: string;
+        slip_id: string;
+    }
 }
 
 // URL: https://api.adviceslip.com/advice
-// Reponse: {"slip": {"advice":"Enjoy a little nonsense now and then.","slip_id":"160"}}
-export async function randomAdvice(): Promise<Slip> {    
+/*
+Reponse: 
+{ 
+    "slip":{ 
+        "advice":"Enjoy a little nonsense now and then.",
+        "slip_id":"160"
+    }
+}
+*/
+export async function randomAdvice(): Promise<SlipResponse> {
     try {
-        const response: AxiosResponse<Slip> = 
-            await axios.get<Slip>(ADVICE_API_RANDOM_URL);
+        const response: AxiosResponse<SlipResponse> =
+            await axios.get<SlipResponse>(ADVICE_API_RANDOM_URL);
 
         return response.data;
     } catch (error) {
--- src/command/advice.ts
+++ src/command/advice.ts
@@ -4,7 +4,7 @@ export function advice(idsStr: string, query: string): void {
     if (!idsStr && !query) {
         // random    
         randomAdvice()
-            .then((r) => console.log(r.slip.advice));
+            .then((response) => console.log(response.slip.advice));
     }
 
     // ids
@@ -16,5 +16,5 @@ export function advice(idsStr: string, query: string): void {
 }
 
 function convertStrIds(value: string): number[] {
-    return value.split(',').map(value => parseInt(value));
+    return value.split(',').map(x => parseInt(x));
 }
