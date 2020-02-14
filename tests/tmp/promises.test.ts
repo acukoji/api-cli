6b4a1e9c1492c77e267eb5b23c6c4f9791edbfd3
Acukoji <acukoji@gmail.com>
2020-02-13 18:05:43 -0700

    added promise.race test

--- tests/tmp/promises.test.ts
+++ tests/tmp/promises.test.ts
@@ -1,14 +1,19 @@
+import { promises } from "dns";
+
 test('promise returns 3 value', () => {
     const promise = Promise.resolve(3);
     return promise.then(value => expect(value).toEqual(3));
 });
 
+
 test('promise returns a [1, 3] array', () => {
     const promise = Promise.resolve([1, 3]);
+    const promise2 = new Promise(resolve => resolve([1, 3]));
     return promise
         .then(value => expect(value).toEqual([1, 3]));
 });
 
+//would there ever be a practical case to have a test like this?
 test('promise returns "expected error" error', () => {
     const promise = Promise.reject(Error('expected error'));
     return promise
@@ -16,6 +21,19 @@ test('promise returns "expected error" error', () => {
         .catch(err => expect(err).toEqual(Error('expected error')));
 });
 
+
+// This works but does this version of the above test make any sense?
+test('Promise returns "expected error" error', () => {
+    const promise = Promise.resolve('expected error');
+    return promise
+        .then(value => expect(value).toEqual('expected error'))
+        .catch(err => fail());
+});
+
+// Would there ever be a situation that would merit having
+// a promise that just thows errors?
+// still not totally clear on situation to throw vs reject error
+// also, is there unnecessary {} in lines 43 and 45?
 test('handle thrown errors in a Promise', () => {
     const promise = new Promise(() => {
         throw Error('expected error');
@@ -28,6 +46,10 @@ test('handle thrown errors in a Promise', () => {
         });
 });
 
+//why does this one not have a test name?
+//it still passes the test?
+//Ans: this is a function that is being used in tests below
+//missing semi-colons are ok?
 
 const oddsFail = (n: number): Promise<string> => {
     return new Promise((resolve, reject) => {
@@ -38,6 +60,22 @@ const oddsFail = (n: number): Promise<string> => {
         }
     })
 }
+
+// above method labeled as a test (nested within a test)
+// i guess functions should not be nested in a test,
+// test should just be test, for readability and also
+// so that the function can be accessed by other tests?
+test('odds fail', () => {
+    const oddsFail = (n: number): Promise<string> => {
+        return new Promise((resolve, reject) => {
+            if (n % 2 == 0) {
+                resolve('success')
+            } else {
+                reject(Error('fail'))
+            }
+        });
+    };
+});
 
 test('create a promise that succeeds for a given even number', () => {
     return oddsFail(10)
@@ -63,8 +101,29 @@ test('call oddsFail using Promise.all', () => {
     return Promise.all([succesCase, failCase]);
 });
 
-test('chain 3 promises together', () => {
+//Promise.race([
+//    recordVideoOne,
+//    recordVideoTwo,
+//    recordVideoThree
+//]).then((message) => {
+//    console.log(message)
+// })
+test('call Promise.race and assert the fastest', () => {
+    const one = new Promise((resolve, reject) => {
+        const timeToWaitMs = 1;
+        setTimeout(() =>resolve(1), timeToWaitMs);
+    });
+    const ten = new Promise(resolve => setTimeout(() => resolve(10), 10));
+    const twenty = new Promise(resolve => setTimeout(() => { resolve(20) }, 20));
+
+    return Promise.race([
+        one,
+        ten,
+        twenty
+    ]).then(value => expect(value).toEqual(1));
 });
+
+test('chain 3 promises together', () => {});
 
 test('create oddsFail example using async/await', () => {
 });
