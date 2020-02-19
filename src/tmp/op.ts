8a20fab3befab6330b8625452bf3d5e58131366d
Acukoji <acukoji@gmail.com>
2020-02-18 18:37:48 -0700

    reviewing op.ts

--- src/tmp/op.ts
+++ src/tmp/op.ts
@@ -1,4 +1,4 @@
-function multipy(a: number, b: number): number {
+function multiply(a: number, b: number): number {
     return a * b;
 }
 
@@ -14,12 +14,28 @@ function divide(a: number, b: number): number {
     return a / b;
 }
 
-function calculate(op: (x: number, y: number) => number, a: number, b: number): number {
-    return op(a, b);
+function calculate(
+    op: (x: number, y: number) => number, // op to be calculated
+    a: number, // first number
+    b: number  // second number
+): number {
+    // do some logic here
+    // const a1 = a.toFixed(13);
+
+    const result = op(a, b);
+
+    // do some logic here
+    // const result1 = result*3;
+    return result;
 }
 
 //to run 
 // ts-node src/tmp/op.ts
+console.log(calculate(multiply, 10, 10)); // 100
+console.log(multiply(10, 10));
 
-console.log(calculate(multipy, 10, 10)); // 100
+
 console.log(calculate(divide, 10, 2));   // 5
+console.log(divide(10, 2));
+
+//console.log(calculate()
